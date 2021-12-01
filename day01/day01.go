488458cd8d486724dca76f41a21f733c4ffef616
Lena <lenahoinkis@Lenas-MacBook-Air.local>
2021-12-01 11:06:47 +0100

    day01 solved

--- day01/day01.go
+++ day01/day01.go
@@ -10,10 +10,28 @@ import (
 var inputFile = flag.String("inputFile", "day01.input", "Relative file path to use as input.")
 
 func main() {
-	int, err := utils.ReadLinesOfInt(*inputFile)
+	depths, err := utils.ReadLinesOfInt(*inputFile)
 	if err != nil {
 		fmt.Print(err)
 	}
-	fmt.Print(int)
+	// Part 1
+	count := 0
+	for i := 1; i < len(depths); i++ {
+		if depths[i] > depths[i-1] {
+			count++
+		}
+	}
+	fmt.Println(count)
 
+	//Part 2
+	count = 0
+	var sumOld int
+	for i := 2; i < len(depths); i++ {
+		sumNew := depths[i] + depths[i-1] + depths[i-2]
+		if i > 2 && sumNew > sumOld {
+			count++
+		}
+		sumOld = sumNew
+	}
+	fmt.Println(count)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/lenahoinkis/AdventOfCode2021
 
-go 1.16
+go 1.17
