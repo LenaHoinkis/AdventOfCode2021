01ead49afb8f62870c4e2bf21f69a5e4cd6aa308
Lena <lena.hoinkis@sap.com>
2021-12-04 11:39:25 +0100

    Day03 part 2 (#1)
    * part02 very ugly
    
    * make function

--- day03/day03.go
+++ day03/day03.go
@@ -8,7 +8,7 @@ import (
 	"github.com/lenahoinkis/AdventOfCode2021/utils"
 )
 
-var inputFile = flag.String("inputFile", "day03ex.input", "Relative file path to use as input.")
+var inputFile = flag.String("inputFile", "day03.input", "Relative file path to use as input.")
 
 func main() {
 	lines, err := utils.ReadLines(*inputFile)
@@ -41,4 +41,47 @@ func main() {
 	g, _ := strconv.ParseInt(gamma, 2, 64)
 	e, _ := strconv.ParseInt(epsilon, 2, 64)
 	fmt.Println(g * e)
+
+	//part2
+	var oxygen, scrubber int64
+	oxygen = calcValue(lines, false)
+	scrubber = calcValue(lines, true)
+	fmt.Println(oxygen * scrubber)
+}
+
+func calcValue(lines []string, takeZeros bool) int64 {
+	var x int64
+	for i := 0; i < len(lines[0])+1; i++ {
+		if len(lines) == 1 {
+			fmt.Println(lines)
+			x, _ = strconv.ParseInt(lines[0], 2, 64)
+			break
+		}
+		var zeros, ones []string
+		for y := 0; y < len(lines); y++ {
+			if string(lines[y][i]) == "0" {
+				zeros = append(zeros, lines[y])
+			} else {
+				ones = append(ones, lines[y])
+			}
+		}
+		if takeZeros {
+			if len(zeros) < len(ones) {
+				lines = zeros
+			} else if len(zeros) > len(ones) {
+				lines = ones
+			} else {
+				lines = zeros
+			}
+		} else {
+			if len(zeros) > len(ones) {
+				lines = zeros
+			} else if len(zeros) < len(ones) {
+				lines = ones
+			} else {
+				lines = ones
+			}
+		}
+	}
+	return x
 }
